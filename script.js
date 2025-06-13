049f999a82336d42c6965bc0b2e04ea8bdd4ac17
Tharass Imus <tobyhogan44@gmail.com>
2025-06-13 16:38:49 +0100

    colors look better

--- script.js
+++ script.js
@@ -168,9 +168,10 @@ class CircleCanvas {
         this.ctx.closePath();
         this.ctx.fillStyle = color;
         this.ctx.fill();
-        this.ctx.strokeStyle = 'white';
-        this.ctx.lineWidth = 0.0001;
-        this.ctx.stroke();
+        // Make the white line between segments visible and thin
+        this.ctx.strokeStyle = '#888888';
+        this.ctx.lineWidth = 0.5; // Thin white line
+        //this.ctx.stroke();
     }
     
     drawSectors() {
@@ -226,12 +227,12 @@ class CircleCanvas {
 
 // Example colors
 const summer1 = "#ffff00";
-const summer2 = "#ffffdd";
+const summer2 = "#ffa8a8";
+const winter2 = "#ffa8a8";
 const winter1 = "#0000ff";
-const winter2 = "#ffffff";
 
 // Create the canvas instance with number of markers (e.g. 32)
-const canvas1 = new CircleCanvas('circleCanvas1', 'markerYValueData1', summer1, summer2, winter1, winter2, 48);
+const canvas1 = new CircleCanvas('circleCanvas1', 'markerYValueData1', summer1, summer2, winter1, winter2, 32);
 
 // Example of how to update a specific canvas
 // To be used for future functionality:

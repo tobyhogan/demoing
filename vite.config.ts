309410c03f68b91d932da5ee2e581e25d06901fa
Tharass Imus <tobyhogan44@gmail.com>
2025-07-12 12:44:14 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react': 'react',
+      'react-dom': 'react-dom'
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 })
